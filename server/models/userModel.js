f454d4c975fc6c574c5b2c471f69742c452d727e
Valentina P <valentina.praninskiene@gmail.com>
2023-11-03 14:42:01 +0200

    Register, login, logout, backend done

---
+++ server/models/userModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    fname: {
+		type: String,
+		required: true,
+	},
+	lname: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+});
+
+ const User = mongoose.model("User", userSchema);
+
+ module.exports = User;
--- server/server.js
+++ server/server.js
@@ -6,14 +6,26 @@ const express = require("express");
 
 const server = express();
 const cors = require("cors");
-
+const cookieParser = require('cookie-parser');
 const connectToDB = require("./config/database");
+
 const bookingsController = require("./contollers/bookingController")
+const usersController = require("./contollers/usersController");
+const requireAuth = require(".//middleware/requireAuth");
 
 server.use(express.json());
-server.use(cors());
+server.use(cookieParser());
+server.use(cors({
+    origin: true,
+    credentials: true,
+}));
 
 connectToDB();
+
+server.post("/register", usersController.register);
+server.post("/login", usersController.login);
+server.get("/logout", usersController.logout);
+server.get("/check-auth", requireAuth, usersController.checkAuth);
 
 server.get("/bookings", bookingsController.fetchBookings );
 server.get("/bookings/:id", bookingsController.fetchBooking);
